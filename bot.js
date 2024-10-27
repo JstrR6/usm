83a8d0799c45f960fe8d13bdabed3042b8ff8d56
JstrR6 <tigerspires13@gmail.com>
2024-10-27 22:27:15 +0000

    Update

--- bot.js
+++ bot.js
@@ -5,14 +5,12 @@ const mongoose = require('mongoose');
 const Member = require('./models/Member'); // Ensure this path is correct
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-});
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Create a new client instance
 const client = new Client({
